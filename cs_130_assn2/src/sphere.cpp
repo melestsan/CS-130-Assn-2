ce5c1a752c9f0edd1ed2ce5276387e7becc4c5e6
Melissa Santos <msant035@ucr.edu>
2017-11-27 15:37:16 -0800

    Sphere Intersection Implemented

--- cs_130_assn2/src/sphere.cpp
+++ cs_130_assn2/src/sphere.cpp
@@ -5,7 +5,24 @@
 // Determine if the ray intersects with the sphere
 bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
 {
-    // TODO
+    vec3 w = ray.direction;
+    vec3 u = ray.endpoint;
+    vec3 v = u - center;
+    
+    double dis = pow(dot(w, v), 2) - dot(w, w) * (dot(v, v) - pow(radius,2));
+    
+    if(dis < 0) {
+		return false;
+	}
+	
+	if(dis == 0) {
+		return true;
+	}
+    
+    if(dis > 0) {
+		return true;
+	}
+    
     return false;
 }
 
